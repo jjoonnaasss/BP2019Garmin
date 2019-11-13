2e9d0074ea40a17a214f0a9974f1deaab3b39020
Timon <timon.boehler@gmx.de>
2019-11-13 14:11:56 +0100

    Added jshint config, simple pre-commit hook, fixed style

---
+++ README.md
@@ -0,0 +1,11 @@
+# BP Garmin
+
+## Setup
+
+Install Python3, Node.js 10 and npm.
+
+Install jshint:
+
+    npm install -g jshint
+
+Copy files from git_hooks/ to .git/hooks, mark as executable if needed.
--- start_oauth.js
+++ start_oauth.js
@@ -1,11 +1,11 @@
 exports.handler = function(event, context, callback) {
 
 // Dependencies
-    const request = require('request')
-    const OAuth = require('oauth-1.0a')
-    const crypto = require('crypto')
-    const qs = require('querystring')
-    const fs = require('fs')
+    const request = require('request');
+    const OAuth = require('oauth-1.0a');
+    const crypto = require('crypto');
+    const qs = require('querystring');
+    const fs = require('fs');
 
     const access_rawdata = fs.readFileSync('access.json');
     const access = JSON.parse(access_rawdata);
@@ -19,14 +19,14 @@ exports.handler = function(event, context, callback) {
             return crypto
                 .createHmac('sha1', key)
                 .update(base_string)
-                .digest('base64')
+                .digest('base64');
         },
-    })
+    });
 
     const request_data = {
         url: 'https://connectapi.garmin.com/oauth-service/oauth/request_token',
         method: 'POST',
-    }
+    };
 
     request(
         {
@@ -35,13 +35,13 @@ exports.handler = function(event, context, callback) {
             form: oauth.authorize(request_data),
         },
         function(error, response, body) {
-            const req_data = qs.parse(body)
-            const uri = 'https://connect.garmin.com/oauthConfirm'
-                + '?' + qs.stringify({oauth_token: req_data.oauth_token})
-            console.log("===URL===")
+            const req_data = qs.parse(body);
+            const uri = 'https://connect.garmin.com/oauthConfirm' +
+                '?' + qs.stringify({oauth_token: req_data.oauth_token});
+            console.log("===URL===");
             redirect = uri;
-            console.log(redirect)
-            callback(null, uri)
+            console.log(redirect);
+            callback(null, uri);
         }
-    )
+    );
 };
