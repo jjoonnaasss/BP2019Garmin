75c41feb804997571d003cf4673e62347ea05620
Jonas Wombacher <jowom@gmx.de>
2019-12-05 10:59:15 +0100

    Now able to read properties from ping notification, no matter which type the according fitness data has (e.g. epoch, daily...); signature not correct yet

--- ping/index.js
+++ ping/index.js
@@ -16,10 +16,13 @@ exports.handler = function (event, context, callback) {
 
     //Daten aus der Ping-Notification abspeichern
     var jsonBody = JSON.parse(event.body);
-    var uat = jsonBody.epochs[0].userAccessToken;
-    var startTime = jsonBody.epochs[0].uploadStartTimeInSeconds;
-    var endTime = jsonBody.epochs[0].uploadEndTimeInSeconds;
-    var url = jsonBody.epochs[0].callbackURL;
+
+    var key = Object.keys(jsonBody)[0];
+
+    var uat = jsonBody[key][0].userAccessToken;
+    var startTime = jsonBody[key][0].uploadStartTimeInSeconds;
+    var endTime = jsonBody[key][0].uploadEndTimeInSeconds;
+    var url = jsonBody[key][0].callbackURL;
     console.log("\n uat: " + uat + "\n startTime: " + startTime + "\n endTime: " + endTime + "\n url:" + url);
 
     const res = {
