e181f70b16fb584fc36d31a9b4078c393ddeaedb
Kyu <kyuhwan7@googlemail.com>
2019-12-20 19:02:37 +0100

    added comments in odv_converter

--- odv_converter/odv_converter.js
+++ odv_converter/odv_converter.js
@@ -5,9 +5,9 @@ module.exports.odvConverter = function(dataJSON, type) {
 
     var AWS = require("aws-sdk");
     AWS.config.update({region: "us-east-2"});
-    var ddb = new AWS.DynamoDB({apiVersion: "2012-08-10"}); //Datenbank-Objekt initialisieren
+    var ddb = new AWS.DynamoDB({apiVersion: "2012-08-10"}); // Initialize data bank object
 
-    //herzfrequenz, stress, schritte ,gewicht, akitivitäten, schlaf, rest egal
+    // heart frequency, stress, steps(?), weight, activities, sleep
 
     var activeTime = 0;
     var moderate = 0;
@@ -15,15 +15,22 @@ module.exports.odvConverter = function(dataJSON, type) {
     // durationInSeconds(manually)
     duration = 0;
 
+    // This is the array, which will be returned in the end. It contains all the Table items of the given summary.
     var params = [];
-    // If you need a String instead of an JSON object, just use JSON.stringify() before the initialized JSON object
+    /* If you need a String instead of an JSON object, just use JSON.stringify() before the initialized JSON object
+    *  type: The type of the summary, which is given as a parameter
+    *  It's mostly the same procedure. Create a new Item for the Table FitnessData, which contains the summaryId, timestamp,
+    *  type of the summary and vaultEntry data. Each vaultEntry is saved separately in a new Item.
+    *  You can include a vaultEntry anytime by creating a new Item and saving it into the array params afterwards.
+    */
     switch(type) {
+        // Daily Summaries Table
         case "dailySum":
-            // Daily Summaries Table
             var daily1 = {
                 TableName: "FitnessData",
                 Item: {
                     summaryId: dataJSON.summaryId,
+                    // TODO: Find out the correct timestamp data.
                     timestamp: Date.now(),
                     type: type,
                     vaultEntry: { VaultEntryType: "HEART_RATE", Value: dataJSON.averageHeartRateInBeatsPerMinute, ValueExtension: null }
@@ -43,8 +50,8 @@ module.exports.odvConverter = function(dataJSON, type) {
             params.push(daily2);
             return params;
 
+        // Third Party Daily Summaries Table
         case "thirdParty":
-            // Third Party Daily Summaries Table
             var third = {
                 TableName: "FitnessData",
                 Item: {
@@ -60,6 +67,7 @@ module.exports.odvConverter = function(dataJSON, type) {
 
             return third;
 
+        // Manually Updated Activity Summaries
         case "manually":
             var manu = {
                 TableName: "FitnessData",
@@ -99,8 +107,10 @@ module.exports.odvConverter = function(dataJSON, type) {
             };
             params.push(actD2);
 
-            // die sample daten
+            // The given sample data of the Activity Details Summaries.
+            // Check if there are any sample data.
             if(dataJSON.hasOwnProperty("samples")) {
+                // Iterate through the sample data, which is saved inside an array.
                 for(var j = 0; j < dataJSON.samples.length; j++) {
                     var actD3 = {
                         TableName: "FitnessData",
@@ -142,7 +152,6 @@ module.exports.odvConverter = function(dataJSON, type) {
                     vaultEntry: { VaultEntryType: "SLEEP_LIGHT", Value: dataJSON.lightSleepDurationInSeconds, ValueExtensions: null }
                 }
             };
-            params.push(sleep1);
             var sleep2 = {
                 TableName: "FitnessData",
                 Item: {
@@ -152,7 +161,6 @@ module.exports.odvConverter = function(dataJSON, type) {
                     vaultEntry: { VaultEntryType: "SLEEP_REM", Value: dataJSON.remSleepInSeconds, ValueExtensions: null }
                 }
             };
-            params.push(sleep2);
             var sleep3 = {
                 TableName: "FitnessData",
                 Item: {
@@ -162,6 +170,8 @@ module.exports.odvConverter = function(dataJSON, type) {
                     vaultEntry: { VaultEntryType: "SLEEP_DEEP", Value: dataJSON.deepSleepDurationInSeconds, ValueExtensions: null }
                 }
             };
+            params.push(sleep1);
+            params.push(sleep2);
             params.push(sleep3);
             return params;
 
