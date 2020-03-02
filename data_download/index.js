fa8c0c76acd21aacd88d7d060412c4e22d75e8cd
Kyu <kyuhwan7@googlemail.com>
2020-03-02 11:42:26 +0100

    added needed if-branch(jonas)

--- data_download/index.js
+++ data_download/index.js
@@ -102,28 +102,28 @@ exports.handler = function (event, context, callback) {
                             if (userData) { //convert all entries to the OpenDataVault-format and append them to the fileData
                                 let daily = [];
                                 let entries = [];
-                                userData.forEach(function (item){
-                                    if(item.sumType.S == "dailies") {
-                                        daily.push(encryption.encryption(item.data.S, access.dataEncPW, true));
-                                    }
-                                    else {
-                                        entries = converter.odvConverter(JSON.parse(encryption.encryption(item.data.S, access.dataEncPW, true)), item.sumType.S); //decrypt the fitness data and give it to the odv_converter
-                                        entries.forEach(function (entry) {
-                                            fileData += JSON.stringify((entry.Item)) + ",";
-                                        });
+                                userData.forEach(function (item) {
+                                    if (item && item.sumType.S && item.data.S) {
+                                        if (item.sumType.S == "dailies") {
+                                            daily.push(encryption.encryption(item.data.S, access.dataEncPW, true));
+                                        } else {
+                                            entries = converter.odvConverter(JSON.parse(encryption.encryption(item.data.S, access.dataEncPW, true)), item.sumType.S); //decrypt the fitness data and give it to the odv_converter
+                                            entries.forEach(function (entry) {
+                                                fileData += JSON.stringify((entry.Item)) + ",";
+                                            });
+                                        }
                                     }
                                 });
-                                for(var i = 0; i < daily.length; i++) {
-                                    for(var j = 0; j < daily.length; j++) {
-                                        if(i != j) {
-                                            if(daily[i].startTimeInSeconds == daily[j].startTimeInSeconds && daily[i].startTimeOffsetInSeconds == daily[j].startTimeOffsetInSeconds) {
-                                                if(daily[i].durationInSeconds >= daily[j].durationInSeconds) {
+                                for (var i = 0; i < daily.length; i++) {
+                                    for (var j = 0; j < daily.length; j++) {
+                                        if (i != j) {
+                                            if (daily[i].startTimeInSeconds == daily[j].startTimeInSeconds && daily[i].startTimeOffsetInSeconds == daily[j].startTimeOffsetInSeconds) {
+                                                if (daily[i].durationInSeconds >= daily[j].durationInSeconds) {
                                                     entries = converter.odvConverter(JSON.parse(daily[i]), "dailies"); //decrypt the fitness data and give it to the odv_converter
                                                     entries.forEach(function (entry) {
                                                         fileData += JSON.stringify((entry.Item)) + ",";
                                                     });
-                                                }
-                                                else {
+                                                } else {
                                                     entries = converter.odvConverter(JSON.parse(daily[j]), "dailies"); //decrypt the fitness data and give it to the odv_converter
                                                     entries.forEach(function (entry) {
                                                         fileData += JSON.stringify((entry.Item)) + ",";
